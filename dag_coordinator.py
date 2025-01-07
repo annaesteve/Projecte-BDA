399970cf035fb88b3b50c9fb3ed694bed076afda
maiteblasi <113500169+maiteblasi@users.noreply.github.com>
2025-01-07 16:00:07 +0100

    Update dag_coordinator.py

--- dag_coordinator.py
+++ dag_coordinator.py
@@ -11,16 +11,26 @@ def wait_for_dags():
     print("Waiting for the data collection DAGs to finish...")
     sleep(10*60)
 
+default_args = {
+    'owner': 'airflow',
+    'retries': 3,
+    'retry_delay': timedelta(seconds=10),
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'email': user_email,
+    'on_failure_callback': task_failure_alert,
+    'on_success_callback': task_success_alert
+}
 
 with DAG(
-    'dag_coordinador',
-    schedule_interval='@daily',  # Ejecuta diariamente
+    dag_id='dag_coordinador',
+    default_args=default_args
+    schedule_interval='@daily',
     start_date=days_ago(1),
     catchup=False,
     description="Pipeline to run data collection, formatting, and modeling tasks"
 ) as dag:
 
-    # Tareas que disparan otros DAGs
     trigger_dag_income = TriggerDagRunOperator(
         task_id='trigger_dag_income',
         trigger_dag_id='dag_income',
@@ -41,14 +51,9 @@ with DAG(
         python_callable=wait_for_dags
     )
     
-    
-    
-    # Tarea final que depende de las otras
     trigger_dag_modeling = TriggerDagRunOperator(
         task_id='trigger_dag_modeling',
         trigger_dag_id='dag_modeling',
     )
     
-    
-    # Una vez que los otros DAGs hayan finalizado, dispara el DAG de modelado
     [trigger_dag_housing, trigger_dag_idealista, trigger_dag_income] >> wait_for_dags >> trigger_dag_modeling
