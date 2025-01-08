cbaa2c6b0283fe30b7baa54ee2eb0fad5ae22ef1
alba-us <150849045+alba-us@users.noreply.github.com>
2025-01-08 11:22:50 +0100

    Update dag_coordinator.py

--- dag_coordinator.py
+++ dag_coordinator.py
@@ -2,14 +2,9 @@ from airflow import DAG
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
-from time import sleep
 
 from metadata import user_email
 
-
-def wait_for_dags():
-    print("Waiting for the data collection DAGs to finish...")
-    sleep(10*60)
 
 default_args = {
     'owner': 'airflow',
@@ -43,15 +38,10 @@ with DAG(
         task_id='trigger_dag_idealista',
         trigger_dag_id='dag_idealista',
     )
-
-    wait_for_dags = PythonOperator(
-        task_id='wait_for_dags',
-        python_callable=wait_for_dags
-    )
     
     trigger_dag_modeling = TriggerDagRunOperator(
         task_id='trigger_dag_modeling',
         trigger_dag_id='dag_modeling',
     )
     
-    [trigger_dag_housing, trigger_dag_idealista, trigger_dag_income] >> wait_for_dags >> trigger_dag_modeling
+    [trigger_dag_housing, trigger_dag_idealista, trigger_dag_income] >> trigger_dag_modeling
